6480d52ed2c66397bac0cd3e06fb79d78979eb85
Cristi Leca <leca_cristi@yahoo.com>
2025-12-04 16:16:13 +0200

    dashboard is done but i forgot to change the branch , i added store card bundles and some little animations

---
+++ src/app/store/page.tsx
@@ -0,0 +1,7 @@
+import Store from "@/components/@pages/Store";
+
+const Page = () => {
+    return <Store/>;
+}
+
+export default Page;
--- src/components/@pages/Register/index.tsx
+++ src/components/@pages/Register/index.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 export default function RegisterPage() {
